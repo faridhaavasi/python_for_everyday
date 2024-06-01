7d091c5c517d82f2af84e74e6f27681d63789a7f
faridhaavasi <faridhavasi777@gmail.com>
2024-06-01 16:23:59 +0330

    add big o

--- tow_sum.py
+++ tow_sum.py
@@ -1,5 +1,9 @@
 #python3
 #tow sum ---> index of tow sum
+'''
+big o ---> o(n^2)
+
+'''
 l = [5,2,9,7] 
 target = 9
 
@@ -7,3 +11,5 @@ for index, value in enumerate(l):
     for i in range(index+1, len(l)):
         if value + l[i] == target:
             print(index,i)
+
+
